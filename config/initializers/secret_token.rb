ee219a2749bddfb90291af6514a299caa1473f06
Joel Braunold <braunold@gmail.com>
2013-10-28 04:25:05 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZionismApp::Application.config.secret_key_base = 'e598abaa6858ff76e01532506b70efa5b3611f48ac4c5772c1a29c3b671d69c596d0d86641baa7608d5e8d4f55b7dc90a235bfca6c099aa34d3971460a7ed9bf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZionismApp::Application.config.session_store :cookie_store, key: '_zionism_app_session'
